4dcae5ebc34f9453fc6151d5ab90d8812db264f9
Rene Eichhorn <rene.eichhorn1@gmail.com>
2015-02-15 15:49:15 +0100

    added more functionality to the texture mapper

--- debug/texture-mapper.js
+++ debug/texture-mapper.js
@@ -11,27 +11,37 @@ TextureMapper.prototype = {
                     "<div class='form-group'>" +
                         "<div class='input-group'>" +
                             "<div class='input-group-addon'>X</div>" +
-                            "<input type='text' id='texture-mapper-x' />" +
+                            "<input type='text' class='form-control' id='texture-mapper-x' />" +
+                            "<div class='input-group-addon'>0.0</div>" +
                         "</div>" +
                         "<div class='input-group'>" +
                             "<div class='input-group-addon'>Y</div>" +
-                            "<input type='text' id='texture-mapper-y' />" +
+                            "<input type='text' class='form-control' id='texture-mapper-y' />" +
+                            "<div class='input-group-addon'>0.0</div>" +
                         "</div>" +
                         "<div class='input-group'>" +
                             "<div class='input-group-addon'>Width</div>" +
-                            "<input type='text' id='texture-mapper-w' />" +
+                            "<input type='text' class='form-control' id='texture-mapper-w' />" +
+                            "<div class='input-group-addon'>0.0</div>" +
                         "</div>" +
                         "<div class='input-group'>" +
                             "<div class='input-group-addon'>Height</div>" +
-                            "<input type='text' id='texture-mapper-h' />" +
+                            "<input type='text' class='form-control' id='texture-mapper-h' />" +
+                            "<div class='input-group-addon'>0.0</div>" +
                         "</div>" +
                     "</div>" +
                 "</form>" +
 
                 "<form class='form-horizontal'>" +
-                    "<div class='input-group'>" +
-                        "<div class='input-group-addon'>Texture</div>" +
-                        "<input type='text' id='texture-mapper-texture' />" +
+                    "<div class='form-group'>" +
+                        "<div class='input-group'>" +
+                            "<div class='input-group-addon'>Texture</div>" +
+                            "<input type='text' class='form-control' id='texture-mapper-texture' />" +
+                            "<span class='input-group-btn'>" +
+                                "<button id='load-texture' class='btn btn-default' type='button'>Load</button>" +
+                                "Load" +
+                            "</span>" +
+                        "</div>" +
                     "</div>" +
                 "</form>" +
             "</div>";
@@ -42,6 +52,20 @@ TextureMapper.prototype = {
             width: 1200,
             height: 800
         });
-        $("#dialog img").cropper({});
+
+        $("#dialog img").cropper({
+            done: function(data) {
+                $("#texture-mapper-x").val(Math.round(data.x));
+                $("#texture-mapper-y").val(Math.round(data.y));
+                $("#texture-mapper-w").val(Math.round(data.width));
+                $("#texture-mapper-h").val(Math.round(data.height));
+            }
+        });
+
+        $("#load-texture").on('click', function() {
+            $("#dialog img").cropper("replace", $("#texture-mapper-texture").val());
+        });
+
+        $("#texture-mapper-texture").val("../data/textures/test_big.jpg");
     }
 };
